f4640801d65691f569c439afd399d045771397bf
Nguyễn Hoàng Phúc <phucnhhe173073@fpt.edu.vn>
2026-02-01 15:25:22 +0700

    Update AuthNavigator

--- src/navigation/AuthNavigator.js
+++ src/navigation/AuthNavigator.js
@@ -1,1 +1,27 @@
-// Auth Navigator - Authentication flow navigation
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreen from '../screens/Auth/Login/LoginScreen';
+import ForgotPasswordScreen from '../screens/Auth/ForgotPasswordScreen';
+
+
+const Stack = createNativeStackNavigator();
+
+export function AuthNavigator() {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <Stack.Screen
+        name="Login"
+        component={LoginScreen}
+      />
+      <Stack.Screen
+        name="ForgotPassword"
+        component={ForgotPasswordScreen}
+      />
+      
+    </Stack.Navigator>
+  );
+}
