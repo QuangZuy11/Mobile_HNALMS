ab01401a957cba36d21944d05e4fe73bb237fccb
Nguyễn Hoàng Phúc <phucnhhe173073@fpt.edu.vn>
2026-02-03 18:56:05 +0700

    Add phone help

---
+++ src/utils/phoneHelper.js
@@ -0,0 +1,24 @@
+import { Alert, Linking } from 'react-native';
+
+// Số điện thoại quản lý - có thể lấy từ API hoặc config
+export const MANAGER_PHONE = '0399053755';
+
+/**
+ * Mở app gọi điện với số điện thoại quản lý
+ */
+export const handleCallManager = () => {
+  const phoneUrl = `tel:${MANAGER_PHONE}`;
+  
+  Linking.canOpenURL(phoneUrl)
+    .then((supported) => {
+      if (supported) {
+        return Linking.openURL(phoneUrl);
+      } else {
+        Alert.alert('Lỗi', 'Không thể mở ứng dụng gọi điện');
+      }
+    })
+    .catch((err) => {
+      console.error('Error opening phone app:', err);
+      Alert.alert('Lỗi', 'Không thể thực hiện cuộc gọi');
+    });
+};
